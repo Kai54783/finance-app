86e386a7b2fc4572699f33ba3e7ffdbf29994f77
Kai54783 <a48807699@gmail.com>
2025-02-03 13:30:21 -0700

    add vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Vercel 需要這個來識別 build 輸出
+  },
+  server: {
+    historyApiFallback: true, // 讓 Vercel 知道要將所有路由指向 index.html
+  },
+});
